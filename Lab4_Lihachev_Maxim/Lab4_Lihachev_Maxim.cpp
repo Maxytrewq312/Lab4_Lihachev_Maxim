db16b4e2d188a614c4b2d885ba887cc853f3f1e5
Максим <@DESKTOP-TSPOI6V>
2020-10-15 18:30:08 +0300

    Добавьте файлы проекта.

---
+++ Lab4_Lihachev_Maxim/Lab4_Lihachev_Maxim.cpp
@@ -0,0 +1,21 @@
+﻿// Lab4_Lihachev_Maxim.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int a;
+	cout << "Введите ваше число: ";
+	cin >> a;
+	cout << "Числа, на которые делится ваше число: " << endl;
+	for (int i = 1; i <= 1000; i++)
+	{
+		if (a % i == 0)
+		{
+			cout << i <<  endl;
+		}
+	}
+	system("pause");
+}
---
+++ Задание4/Задание4.cpp
@@ -0,0 +1,52 @@
+﻿// Задание4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+using namespace std;
+int main()
+{ 
+	setlocale(0, "");
+	double f1 = 1;
+	double f2 = 1;
+	double f3 = 1;
+	double f4 = 1;
+	double x;
+	int s = 3;
+	int d = 5;
+	int g = 7;
+	cout << "Введите значение x: " << endl;
+	cin >> x;
+	double n;
+	cout << "Введите значение n: " << endl;
+	cin >> n;
+	double e = 2 * n + 1;
+	for (s; s > 0; s--)
+	{
+		f1 *= s;
+
+	}
+	for (d; d > 0; d--)
+	{
+		f2 *= d;
+
+	}
+	for (g; g > 0; g--)
+	{
+		f3 *= g;
+
+	}
+	for (e; e > 0; e--)
+	{
+		f4 *= e;
+
+	}
+	for (int i = 0; i < n; i++)
+	{
+		x = ((pow(x, 3)) / f1) + ((pow(x, 5)) / f2) - ((pow(x, 7)) / f3) + (pow(-1, n) * pow(x, 2*n + 1)) / f4 - sin(x);
+	}
+	cout << "Значение синуса: " << x << endl;
+
+	system("pause");
+}
